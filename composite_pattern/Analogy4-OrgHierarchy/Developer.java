39ea0ec7a40ad2aec127ab8deb0ebf5dcd3521f0
Dulari Dayananda <dulariwathsala824@gmail.com>
2026-04-27 13:31:24 +0530

    Add Developer and Manager classes implementing Employee interface

---
+++ composite_pattern/Analogy4-OrgHierarchy/Developer.java
@@ -0,0 +1,11 @@
+class Developer implements Employee {
+    private String name;
+
+    public Developer(String name) {
+        this.name = name;
+    }
+
+    public void showDetails() {
+        System.out.println("Developer: " + name);
+    }
+}
---
+++ composite_pattern/Analogy4-OrgHierarchy/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Developer d1 = new Developer("Alice");
+        Developer d2 = new Developer("Bob");
+
+        Manager m = new Manager("John");
+        m.add(d1);
+        m.add(d2);
+
+        m.showDetails();
+    }
+}
